84b0157f0004ba492a3ab151d78733ef18d3afa5
rajesh.pany <rajeshp@mindfiresolutions.com>
2020-04-15 11:10:25 +0530

    Changes In edit profile component

--- src/app/MedSol/edit-profile/edit-profile-photo/edit-profile-photo.component.ts
+++ src/app/MedSol/edit-profile/edit-profile-photo/edit-profile-photo.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialog, MatDialogRef } from '@angular/material';
-import { EditProfileComponent } from './edit-profile.component';
+
 
 @Component({
   selector: 'app-edit-profile-photo',
